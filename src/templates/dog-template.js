0d9656e7f4dd1312372e307343d88acd3857bd06
Gibrán de Jesús Ramón Perera <jipixz97@gmail.com>
2021-04-02 15:29:30 -0500

    Minimal changes

--- src/templates/dog-template.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function DogTemplate({ pageContext: { dog } }) {
-    return (
-        <section>
-            {dog.name} - {dog.breed}
-        </section>
-    )
-}
